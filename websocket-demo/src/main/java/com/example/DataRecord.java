0d298fb9510a52a04dc525bd85b62ad58b774dee
Fedor Vyanckus <vyanckus@mail.ru>
2025-11-03 04:18:20 +0200

    chore: remove legacy modules

--- websocket-demo/src/main/java/com/example/DataRecord.java
+++
@@ -1,9 +0,0 @@
-package com.example;
-
-public record DataRecord(Long time, Double value) {
-
-    @Override
-    public String toString() {
-        return "DataRecord{" + "time=" + time + ", value=" + value + '}';
-    }
-}
